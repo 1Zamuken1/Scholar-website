4fbc81b9b63d03d3a84623dc2062930e7590b03b
1Zamuken1 <juanbarrios072@gmail.com>
2023-06-07 10:28:43 -0500

    fixed look and functionality of login.

--- app/controllers/loginController.php
+++ app/controllers/loginController.php
@@ -22,37 +22,52 @@ class loginController extends Controller {
 
   function post_login()
   {
-    if (!Csrf::validate($_POST['csrf']) || !check_posted_data(['usuario','csrf','password'], $_POST)) {
-      Flasher::new('Acceso no autorizado.', 'danger');
+    try {
+      if (!Csrf::validate($_POST['csrf']) || !check_posted_data(['email','csrf','password'], $_POST)) {
+        Flasher::new('Acceso no autorizado.', 'danger');
+        Redirect::back();
+      }
+  
+      // Data pasada del formulario
+      $email  = clean($_POST['email']);
+      $password = clean($_POST['password']);
+     
+        // Verificar si el email es válido
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+          throw new Exception('El correo electrónico no es válido');
+        }
+
+        // Información del usuario loggeado, simplemente se puede reemplazar aquí con un query a la base de datos
+        // para cargar la información del usuario si es existente
+        $user = 
+        [
+          'id'       => 123,
+          'name'     => 'Bee Default', 
+          'email'    => 'hellow@joystick.com.mx', 
+          'avatar'   => 'myavatar.jpg', 
+          'tel'      => '11223344', 
+          'color'    => '#112233',
+          'user'     => 'bee',
+          'password' => '$2y$10$R18ASm3k90ln7SkPPa7kLObcRCYl7SvIPCPtnKMawDhOT6wPXVxTS'
+        ];
+        
+        if ($usuario !== $user['user'] || !password_verify($password.AUTH_SALT, $user['password'])) {
+          Flasher::new('Las credenciales no son correctas.', 'danger');
+          Redirect::back();
+        }
+
+        // Loggear al usuario
+        Auth::login($user['id'], $user);
+        Redirect::to('home/flash');
+
+      }catch (Exception $e){
+      Flasher::new($e->getMessage(), 'danger');
       Redirect::back();
     }
 
-    // Data pasada del formulario
-    $usuario  = clean($_POST['usuario']);
-    $password = clean($_POST['password']);
-
-    // Información del usuario loggeado, simplemente se puede reemplazar aquí con un query a la base de datos
-    // para cargar la información del usuario si es existente
-    $user = 
-    [
-      'id'       => 123,
-      'name'     => 'Bee Default', 
-      'email'    => 'hellow@joystick.com.mx', 
-      'avatar'   => 'myavatar.jpg', 
-      'tel'      => '11223344', 
-      'color'    => '#112233',
-      'user'     => 'bee',
-      'password' => '$2y$10$R18ASm3k90ln7SkPPa7kLObcRCYl7SvIPCPtnKMawDhOT6wPXVxTS'
-    ];
 
 
-    if ($usuario !== $user['user'] || !password_verify($password.AUTH_SALT, $user['password'])) {
-      Flasher::new('Las credenciales no son correctas.', 'danger');
-      Redirect::back();
-    }
 
-    // Loggear al usuario
-    Auth::login($user['id'], $user);
-    Redirect::to('home/flash');
+
   }
 }
